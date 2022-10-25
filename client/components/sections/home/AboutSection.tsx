9a04b04b8ebae19ba27bf3a2646118255bd7cc4b
Chase <chase.seltz21@gmail.com>
2022-10-25 16:46:05 -0700

    Improve responsive padding for home page's about section

--- client/components/sections/home/AboutSection.tsx
+++ client/components/sections/home/AboutSection.tsx
@@ -9,18 +9,18 @@ const AboutSection = () => {
         backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.6)), url(${bgImage.src})`,
       }}
       className={
-        "py-24 px-12 mt-24 bg-cover text-white text-center text-xl leading-8"
+        "py-24 px-4 md:px-12 mt-24 bg-cover text-white text-center text-xl lg:text-xl leading-8 lg:leading-9"
       }
     >
       <h3 className={"text-5xl mb-8"}>Chase Seltzner</h3>
-      <p className={"mb-6"}>
+      <p className={"mb-6 max-w-[70ch] mx-auto"}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi harum
         ipsa maiores mollitia pariatur perferendis quo rem voluptates
         voluptatibus. Aliquam assumenda beatae consequatur deleniti, distinctio
         dolor ducimus eos exercitationem itaque minima neque nisi odit provident
         quas ratione rem totam voluptatem.
       </p>
-      <p className={"mb-6"}>
+      <p className={"mb-6 max-w-[70ch] mx-auto"}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
         corporis excepturi harum minus neque nobis officiis reiciendis
         similique! A ab alias aliquid atque cupiditate excepturi non odit porro
