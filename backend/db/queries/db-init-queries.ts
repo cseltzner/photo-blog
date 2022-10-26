2ceba534fe577d57cd27c6cadda40948d84ef422
Chase <chase.seltz21@gmail.com>
2022-10-26 10:59:14 -0700

    Add date_created field to database tables

--- backend/db/queries/db-init-queries.ts
+++ backend/db/queries/db-init-queries.ts
@@ -7,9 +7,9 @@ import generateId from "../../utils/generateId";
 const init_db_queries = {
   initDb: `CREATE DATABASE IF NOT EXISTS ${process.env.DATABASE}`,
   initImageTable:
-    "CREATE TABLE IF NOT EXISTS images(id CHAR(10) PRIMARY KEY, cloudinary_public_id VARCHAR(255) NOT NULL, title VARCHAR(255), description TEXT, img_url VARCHAR(1000) NOT NULL, favorite BOOLEAN DEFAULT false, front_page BOOLEAN DEFAULT false, categories TEXT [] )",
+    "CREATE TABLE IF NOT EXISTS images(id CHAR(10) PRIMARY KEY, cloudinary_public_id VARCHAR(255) NOT NULL, title VARCHAR(255), description TEXT, img_url VARCHAR(1000) NOT NULL, favorite BOOLEAN DEFAULT false, front_page BOOLEAN DEFAULT false, categories TEXT [], date_added DATE NOT NULL DEFAULT CURRENT_DATE )",
   initUsersTable:
-    "CREATE TABLE IF NOT EXISTS users(id CHAR(10) PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), role VARCHAR(255))",
+    "CREATE TABLE IF NOT EXISTS users(id CHAR(10) PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), role VARCHAR(255), date_added DATE NOT NULL DEFAULT CURRENT_DATE)",
   createDefaultAdminUser: (encryptedPassword: string) => {
     return `INSERT INTO users (id, username, password, role) VALUES ('${generateId()}', '${
       process.env.DEFAULT_ADMIN_USERNAME
