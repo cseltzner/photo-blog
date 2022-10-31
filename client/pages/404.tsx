8dd2f20de99af23a06bad3375c571d44be5bd41a
Chase <chase.seltz21@gmail.com>
2022-10-31 14:12:45 -0700

    Create 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import frogPhoto from "../assets/404/frog-compressed.jpg";
+import Link from "next/link";
+
+const Custom404 = () => {
+  return (
+    <div className={"flex items-center flex-col gap-4 px-8 py-8 text-4xl"}>
+      <h1>Oops!</h1>
+      <p>Page not found</p>
+      <Link href={"/"}>
+        <a className={"text-blue-700 underline text-3xl mb-2"}>Return home</a>
+      </Link>
+      <img
+        src={frogPhoto.src}
+        width={900}
+        alt="Small frog sitting next to some purple flowers"
+      />
+    </div>
+  );
+};
+
+export default Custom404;
