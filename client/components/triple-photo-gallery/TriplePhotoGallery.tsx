b657b5effa856a47ff70283c466c94e554f7b418
Chase <chase.seltz21@gmail.com>
2022-10-31 14:24:08 -0700

    Improve responsiveness of 'latest favorites' section

--- client/components/triple-photo-gallery/TriplePhotoGallery.tsx
+++ client/components/triple-photo-gallery/TriplePhotoGallery.tsx
@@ -12,15 +12,19 @@ interface Props {
 
 const TriplePhotoGallery = ({ images }: Props) => {
   return (
-    <div className={"flex gap-8 text-center"}>
+    <div className={"flex gap-8 text-center px-2"}>
       {/* Item 1 */}
       <a href={`/favorites/${images[0] && images[0].id}`} className={"group"}>
         <div className="flex flex-col items-center cursor-pointer">
           <img src={images[0] && images[0].img_url} alt="Recent favorite" />
-          <p className={"text-lg mt-3 opacity-70"}>
+          <p className={"text-sm sm:text-lg mt-3 opacity-70"}>
             {moment(images[0] && images[0].date_added).format("MMMM DD YYYY")}
           </p>
-          <h4 className={"font-serif text-3xl mt-1 group-hover:underline"}>
+          <h4
+            className={
+              "font-serif text-xl sm:text-3xl mt-1 group-hover:underline"
+            }
+          >
             {images[0] && images[0].title}
           </h4>
         </div>
@@ -29,10 +33,14 @@ const TriplePhotoGallery = ({ images }: Props) => {
       <a href={`/favorites/${images[1] && images[1].id}`} className={"group"}>
         <div className="flex flex-col items-center cursor-pointer">
           <img src={images[1] && images[1].img_url} alt="Recent favorite" />
-          <p className={"text-lg mt-3 opacity-70"}>
+          <p className={"text-sm sm:text-lg mt-3 opacity-70"}>
             {moment(images[1] && images[1].date_added).format("MMMM DD YYYY")}
           </p>
-          <h4 className={"font-serif text-3xl mt-1  group-hover:underline"}>
+          <h4
+            className={
+              "font-serif text-xl sm:text-3xl mt-1  group-hover:underline"
+            }
+          >
             {images[1] && images[1].title}
           </h4>
         </div>
@@ -41,10 +49,14 @@ const TriplePhotoGallery = ({ images }: Props) => {
       <a href={`/favorites/${images[2] && images[2].id}`} className={"group"}>
         <div className="flex flex-col items-center cursor-pointer">
           <img src={images[2] && images[2].img_url} alt="Recent favorite" />
-          <p className={"text-lg mt-3 opacity-70"}>
+          <p className={"text-sm sm:text-lg mt-3 opacity-70"}>
             {moment(images[2] && images[2].date_added).format("MMMM DD YYYY")}
           </p>
-          <h4 className={"font-serif text-3xl mt-1  group-hover:underline"}>
+          <h4
+            className={
+              "font-serif text-xl sm:text-3xl mt-1  group-hover:underline"
+            }
+          >
             {images[2] && images[2].title}
           </h4>
         </div>
