460e014f4dc004504910e97f08dedccf8b0a0138
Chase <chase.seltz21@gmail.com>
2022-11-18 22:08:02 -0700

    Extract contact page strings

--- client/pages/contact/index.tsx
+++ client/pages/contact/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { contactStrings as strings } from "../../strings/pages/contactStrings";
 
 const Contact = () => {
   // Email encrypted with base64 to avoid crawlers spamming me (hopefully?)
@@ -6,16 +7,20 @@ const Contact = () => {
 
   // Update title
   useEffect(() => {
-    document.title = "Contact | Seltzport";
+    document.title = strings.html_pageTitle;
   }, []);
 
   return (
     <main className={"max-w-[800px] mx-auto pb-24 px-4"}>
-      <h1 className={"text-4xl font-serif border-b"}>Contact me</h1>
+      <h1 className={"text-4xl font-serif border-b"}>
+        {strings.html_mainHeader}
+      </h1>
       <address className={"text-xl mt-12"}>
         {/* Email */}
         <div>
-          <h2 className={"border-b mb-2 text-2xl"}>Email</h2>
+          <h2 className={"border-b mb-2 text-2xl"}>
+            {strings.html_contactHeader1}
+          </h2>
           <a
             href={`mailto:${atob(emailBase64)}`}
             className={"text-blue-700 hover:underline"}
@@ -25,14 +30,16 @@ const Contact = () => {
         </div>
         {/* Github */}
         <div>
-          <h2 className={"border-b mb-2 mt-12 text-2xl"}>Github</h2>
+          <h2 className={"border-b mb-2 mt-12 text-2xl"}>
+            {strings.html_contactHeader2}
+          </h2>
           <a
-            href={"https://www.github.com/cseltzner"}
+            href={strings.html_githubLinkHref}
             target={"_blank"}
             rel="noreferrer"
             className={"text-blue-700 hover:underline"}
           >
-            Github link
+            {strings.html_githubLink}
           </a>
         </div>
       </address>
---
+++ client/strings/pages/contactStrings.ts
@@ -0,0 +1,9 @@
+export const contactStrings = {
+  html_pageTitle: "Contact | Seltzport",
+  html_mainHeader: "Contact me",
+  html_contactHeader1: "Email",
+  html_contactHeader2: "Github",
+
+  html_githubLinkHref: "https://www.github.com/cseltzner",
+  html_githubLink: "Github link",
+};
