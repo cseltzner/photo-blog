55bb2cc74491d29fd3d284adff47a77d7d4b0c2f
Chase <chase.seltz21@gmail.com>
2022-11-18 22:12:13 -0700

    Extract 404 page strings

--- client/pages/404.tsx
+++ client/pages/404.tsx
@@ -1,25 +1,24 @@
 import React, { useEffect } from "react";
 import frogPhoto from "../assets/404/frog-compressed.jpg";
 import Link from "next/link";
+import { _404Strings as strings } from "../strings/pages/404Strings";
 
 const Custom404 = () => {
   // Update title
   useEffect(() => {
-    document.title = "Page not found | Seltzport";
+    document.title = strings.html_pageTitle;
   }, []);
 
   return (
     <div className={"flex items-center flex-col gap-4 px-8 py-8 text-4xl"}>
-      <h1>Oops!</h1>
-      <p>Page not found</p>
+      <h1>{strings.html_mainHeader}</h1>
+      <p>{strings.html_mainText}</p>
       <Link href={"/"}>
-        <a className={"text-blue-700 underline text-3xl mb-2"}>Return home</a>
+        <a className={"text-blue-700 underline text-3xl mb-2"}>
+          {strings.html_returnLink}
+        </a>
       </Link>
-      <img
-        src={frogPhoto.src}
-        width={900}
-        alt="Small frog sitting next to some purple flowers"
-      />
+      <img src={frogPhoto.src} width={900} alt={strings.html_imgAlt} />
     </div>
   );
 };
---
+++ client/strings/pages/404Strings.ts
@@ -0,0 +1,9 @@
+export const _404Strings = {
+  html_pageTitle: "Page not found | Seltzport",
+  html_mainHeader: "Oops!",
+  html_mainText: "Page not found",
+  html_returnLink: "Return home",
+
+  // Alt text
+  html_imgAlt: "Small frog sitting next to some purple flowers",
+};
