6d196c22d35c1e6b2f0da6c5e048270a46d0e32c
Chase <chase.seltz21@gmail.com>
2022-11-25 19:19:51 -0700

    Extract test images to be used for various tests

--- client/__test__/pages/favorites/FavoritesIndex.test.tsx
+++ client/__test__/pages/favorites/FavoritesIndex.test.tsx
@@ -5,36 +5,7 @@ import { favoritesStrings as strings } from "../../../strings/components/favorit
 import { server } from "../../../mocks/server";
 import { rest } from "msw";
 import { apiProxy } from "../../../utils/apiProxy";
-
-const testImages = [
-  {
-    id: 1,
-    img_url: "https://fake.imagesource.com/abc/image/upload/123/abc/abc.jpg",
-    date_added: "2022-11-06 08:10:34.165627+07",
-    title: "image 1",
-    test__month: "November",
-    test__day: "05",
-    test__year: 2022,
-  },
-  {
-    id: 2,
-    img_url: "https://fake.imagesource.com/abcd/image/upload/123/abc/abc.jpg",
-    date_added: "2022-11-07 08:11:34.165627+07",
-    title: "image 2",
-    test__month: "November",
-    test__day: "06",
-    test__year: 2022,
-  },
-  {
-    id: 3,
-    img_url: "https://fake.imagesource.com/abcde/image/upload/123/abc/abc.jpg",
-    date_added: "2022-11-08 08:12:34.165627+07",
-    title: "image 3",
-    test__month: "November",
-    test__day: "07",
-    test__year: 2022,
-  },
-];
+import { testImages } from "../../test-utils/testImages";
 
 describe("Favorites index page", () => {
   beforeEach(() => {
